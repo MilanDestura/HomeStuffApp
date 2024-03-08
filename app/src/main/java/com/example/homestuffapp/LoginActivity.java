274d0a47b7693dbe18cd353e5c5ce4cca96b1c26
abrasile2912 <abrasile2912@gmail.com>
2024-03-08 11:53:16 -0800

    Included LoginActivity.java
    New class was included in the project.

---
+++ app/src/main/java/com/example/homestuffapp/LoginActivity.java
@@ -0,0 +1,58 @@
+package com.example.homestuffapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText loginEmail, loginPassword;
+    private Button loginButton;
+    private TextView signupRedirectText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginEmail = findViewById(R.id.tvEmail);
+        loginPassword = findViewById(R.id.tvPassword);
+        loginButton = findViewById(R.id.btLogin);
+        signupRedirectText = findViewById(R.id.tvSignupRedirectText);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = loginEmail.getText().toString();
+                String pass = loginPassword.getText().toString();
+
+                if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    if (!pass.isEmpty()) {
+                        Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(LoginActivity.this, MenuActivity.class));
+                        finish();
+                    } else {
+                        loginPassword.setError("Password cannot be empty");
+                    }
+                } else if (email.isEmpty()) {
+                    loginEmail.setError("Email cannot be empty");
+                } else {
+                    loginEmail.setError("Please enter a valid email");
+                }
+            }
+        });
+
+        signupRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+            }
+        });
+    }
+}
