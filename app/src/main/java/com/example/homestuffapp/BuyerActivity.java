7766a0e39b2e412d5181b8bacad0000107bbb308
MilanDestura <milan.destura007@gmail.com>
2024-03-08 09:32:22 -0800

    Revert "Removed SearchView Object"
    This reverts commit cdb2dbfaad6ee68161182612c7bb9ea5528c045d.

--- app/src/main/java/com/example/homestuffapp/BuyerActivity.java
+++ app/src/main/java/com/example/homestuffapp/BuyerActivity.java
@@ -42,6 +42,7 @@ public class BuyerActivity extends AppCompatActivity {
     boolean isFiltered = false;
     Bitmap myImg;
 
+    SearchView searchItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
