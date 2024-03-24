dcf1ba29a157ef3822cdcdcfff285c76803f63ed
mauigs <maui.g.sales@gmail.com>
2024-03-23 20:45:17 -0700

    Order history Activity initial commit

--- app/src/main/java/com/example/homestuffapp/OrderHistoryActivity.java
+++ app/src/main/java/com/example/homestuffapp/OrderHistoryActivity.java
@@ -1,13 +1,14 @@
 package com.example.homestuffapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class activity_order_status extends AppCompatActivity {
+public class OrderHistoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_order_status);
+        setContentView(R.layout.activity_order_history);
     }
 }
